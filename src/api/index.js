751f3d1195857a2938f58896860b885badee7ba3
Tarun Sunkara <150528511+Sunkara-Tarun@users.noreply.github.com>
2023-12-09 11:53:50 +0530

    Update index.js
    url update for API change

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({baseURL : 'http://localhost:5500/'})
+const API = axios.create({baseURL : 'https://sunkara-tarun.github.io/Youtubeclone/
 API.interceptors.request.use(req =>{
     if(localStorage.getItem('Profile')){
        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
